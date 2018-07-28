855de48633a0299fcc3552195bb71b4557314e46
RomanACU <alexciur86@gmail.com>
2018-07-28 13:57:46 -0500

    conectar xml con java
    Dar clic en un elemento

--- AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
+++ AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
@@ -2,12 +2,33 @@ package mx.mobilestudio.androidlayoutbasics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //enlazar el xml con java, se crea una variable
+    private TextView center_center_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //método para buscar una vista con id = center_center dentro del xml
+        center_center_tv = (TextView) findViewById(R.id.center_center);
+
+        //cambia valor de texto desde java
+        center_center_tv.setText("Alejandro");
+
+        center_center_tv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        //mensaje al dar clic
+        Toast.makeText(this,"Se le dio click",Toast.LENGTH_LONG).show();
+
     }
 }
