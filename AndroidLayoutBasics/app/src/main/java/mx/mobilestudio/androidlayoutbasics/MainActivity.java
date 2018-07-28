afc6f07c541fabfff88e26b6ab371c1607467fbb
ElizabethMurillo <elimurilloem@gmail.com>
2018-07-28 13:57:41 -0500

    xml y java
    Union de cómo utilizar un objeto de xml en java

--- AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
+++ AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
@@ -2,12 +2,28 @@ package mx.mobilestudio.androidlayoutbasics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView centro_centro_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        centro_centro_tv = (TextView) findViewById(R.id.centro_centro);
+        centro_centro_tv.setText("asdasdasdasdasdasdasd");
+        centro_centro_tv.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick (View view){
+        Toast.makeText(this,"Se le dio Click",Toast.LENGTH_LONG).show();
+
+        }
+
 }
