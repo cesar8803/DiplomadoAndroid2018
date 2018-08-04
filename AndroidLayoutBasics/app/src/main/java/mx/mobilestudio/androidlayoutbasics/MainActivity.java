ccfc9e898e125e2d174758cbfc3be3be28af8659
Ricardo López <ricardo.lopez@mobilestudio.mx>
2018-08-04 10:34:28 -0500

    fin de la practica layous
    fin

--- AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
+++ AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
@@ -1,5 +1,6 @@
 package mx.mobilestudio.androidlayoutbasics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         center_center_tv= (TextView) findViewById(R.id.centro);
-        center_center_tv.setText("animation");
+        //center_center_tv.setText("animation");
         center_center_tv.setOnClickListener(this);
     }
 
@@ -24,10 +25,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
 
         Toast.makeText(this,"Se le dio Click gordo", Toast.LENGTH_LONG).show();
+        center_center_tv.setText("YA DISTE CLICK");
 
+        Intent intent_pantalla_secundaria = new Intent(this, SecondaryActivity.class);
+        startActivity(intent_pantalla_secundaria);
     }
     //.activity_main cambiando eso cambio la ropita de la applicacion
     //TextView es una clase que llama ala objrto center_center_
     //centro lo tome de las palabras fruta etc en activity_main.xlm, es una id.
     //Toast es un metodo estatico el cual no necesita generar una clase para invocarlo.
+
+    //center_center_tv= (TextView) findViewById(R.id.centro); "R." --> R. es una clase que tiene propiedades
+    // es una clase estatica que guarda todos los id que tengo en mi clase, si hay nuevos id se cargar a
+    // a clase R.
+
 }
