7999fa94291d2093eae292beccb0e7c9769d6056
Ricardo López <ricardo.lopez@mobilestudio.mx>
2018-08-11 12:51:22 -0500

    Place finder 2

--- PlaceFinder/app/src/main/java/com/example/mobilestudiolaptop004/placefinder/MainActivity.java
+++ PlaceFinder/app/src/main/java/com/example/mobilestudiolaptop004/placefinder/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.mobilestudiolaptop004.placefinder;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Response.Listener, Response.ErrorListener {
 //Volley --> libreria para trabajar http, esta es de google
 
-    private Button centro;
+    private Button boton1;
     private Button boton2;
 
 
@@ -21,20 +27,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        centro= findViewById(R.id.B1);
-        centro.setOnClickListener(this);
 
-        boton2= findViewById(R.id.B2);
-        boton2.setOnClickListener(this);
+        boton1= findViewById(R.id.B1);
+        boton1.setOnClickListener(this);
 
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId()==R.id.B1 && centro.getText().toString() !="x") {
-            centro.setText("x");
-            Toast.makeText(this, "Se le dio Click gordo", Toast.LENGTH_LONG).show();
-        }
+        callFourSquareApi("gasolinera");
+    }
+
+
+    //Metodo para ejecutar el request HTTP mediante Volley
+    public void callFourSquareApi(String query){
+        String location = "19.395209"+","+"-99.1544203"; //HARDCORE
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String URL = Uri.parse("https://api.foursquare.com/v2/venues/search").buildUpon()
+                .appendQueryParameter("client_id","HOSIY11XMXHWFADXIPQTF5HRZA3YIWIFGRAOA5NIGXOY3CWI")
+                .appendQueryParameter("client_secret","OGATJNY0E0JY15PRXYD5MQ2WW3EMFLRAWFHLAOQYSTMVKMHM")
+                .appendQueryParameter("v","20130815")
+                .appendQueryParameter("ll",location)
+                .appendQueryParameter("query",query).build().toString();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,URL, this,this);
+        queue.add(stringRequest);
+
+    }
+
+    //Este metodo es obligatario
+    @Override
+    public void onResponse(Object response) {
+        Toast.makeText(this,(String)response, Toast.LENGTH_LONG).show();
+
+    }
+    //Este metodo es obligatorio
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
     }
 }
