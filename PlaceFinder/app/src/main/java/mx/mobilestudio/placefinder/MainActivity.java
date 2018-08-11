cdbf2f73d4b46ad126760eb2702efe126578ac80
cesar8803 <hlj.cesar@gmail.com>
2018-08-11 12:50:01 -0500

    Place Finder
    Place Finder

--- PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/MainActivity.java
+++ PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/MainActivity.java
@@ -1,13 +1,21 @@
 package mx.mobilestudio.placefinder;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener , Response.Listener ,Response.ErrorListener {
 
     private Button miBoton;
     private Button miBoton2;
@@ -28,15 +36,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+        callFourSquareApi("gasolinera");
 
-          if( view.getId()==R.id.button && miBoton.getText().toString() != "X" ){
-              miBoton.setText("X");
-              Toast.makeText(this, "Se dio click al boton 1", Toast.LENGTH_LONG).show();
+    }
 
-          }else if(view.getId()==R.id.button2){
-              Toast.makeText(this, "Se dio click al boton 2", Toast.LENGTH_LONG).show();
 
-          }
+    //Metodo para ejecutar el request HTTP mediante Volley
+    public void callFourSquareApi(String  query) {
+
+        String location =  "19.395209"+","+"-99.1544203"; // HARDCODE
+
+        RequestQueue queue  = Volley.newRequestQueue(this);
+
+        String URL = Uri.parse("https://api.foursquare.com/v2/venues/search").buildUpon()
+                        .appendQueryParameter("client_id","HOSIY11XMXHWFADXIPQTF5HRZA3YIWIFGRAOA5NIGXOY3CWI")
+                        .appendQueryParameter("client_secret","OGATJNY0E0JY15PRXYD5MQ2WW3EMFLRAWFHLAOQYSTMVKMHM")
+                         .appendQueryParameter("v", "20130815")
+                        .appendQueryParameter("ll", location)
+                        .appendQueryParameter("query",query).build().toString();
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,URL, this,this);
+
+        queue.add(stringRequest);
+    }
+
+
+    @Override
+    public void onResponse(Object response) {
+
+
+        Toast.makeText(this,(String) response, Toast.LENGTH_LONG ).show();
+    }
+
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
 
     }
 }
