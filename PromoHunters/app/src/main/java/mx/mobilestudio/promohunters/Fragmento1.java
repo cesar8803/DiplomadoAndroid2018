64ea3c25d536fc4baabd32ea71873b099035c75a
jkings <jmanuel.reyes@synergyj.com>
2018-10-27 10:46:48 -0500

    a

---
+++ PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/Fragmento1.java
@@ -0,0 +1,30 @@
+package mx.mobilestudio.promohunters;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragmento1 extends Fragment {
+
+
+    public Fragmento1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragmento1,container,false);
+        return view;
+    }
+
+}
---
+++ PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/PromoFormActivity.java
@@ -0,0 +1,42 @@
+package mx.mobilestudio.promohunters;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class PromoFormActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button1;
+    private Button button2;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promo_form);
+        button1=findViewById(R.id.b1);
+        button1.setOnClickListener(this);
+        button2=findViewById(R.id.b2);
+        button2.setOnClickListener(this);
+        fragmentManager=getFragmentManager();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.b1){
+            fragmentoonline();
+        }
+
+    }
+
+    public void fragmentoonline (){
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        Fragment fragmento1 = new Fragmento1();
+        fragmentTransaction.replace(R.id.activitypromolayout,fragmento1);
+        fragmentTransaction.commit();
+    }
+}
