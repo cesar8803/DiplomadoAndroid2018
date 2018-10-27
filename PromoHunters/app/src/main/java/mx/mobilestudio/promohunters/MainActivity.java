ffb2cb9b448f38dbf87c0017150c291438bcacf6
jkings <jmanuel.reyes@synergyj.com>
2018-10-27 11:16:20 -0500

    Ultimo update Promo hunters
    Ultimo update Promo hunters

---
+++ PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/MainActivity.java
@@ -0,0 +1,25 @@
+package mx.mobilestudio.promohunters;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageButton=findViewById(R.id.Bmas);
+        imageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
