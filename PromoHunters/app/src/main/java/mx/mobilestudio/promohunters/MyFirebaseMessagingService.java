baac739d76b90a10513c589bdd39dcb5fe963685
jkings <jmanuel.reyes@synergyj.com>
2018-11-10 11:16:52 -0600

    se sube el manifest
    se sube el manifest

---
+++ PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/MyFirebaseMessagingService.java
@@ -0,0 +1,18 @@
+package mx.mobilestudio.promohunters;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
